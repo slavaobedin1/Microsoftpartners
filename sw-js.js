6423c27fb7b649c981f74eea07d485465fe2e6f9
slavaobedin1 <slavaobedin1@gmail.com>
2026-03-22 10:44:11 +0200

    Add files via upload

---
+++ sw-js.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = "ms-resellers-v1";
+const urlsToCache = ["/Microsoftpartners/", "/Microsoftpartners/index.html"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
